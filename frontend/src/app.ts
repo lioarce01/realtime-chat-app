8c1afa4a829d11728d1dba83be0266a3b6401606
lioarce01 <lioarce1@gmail.com>
2024-11-19 17:23:17 -0300

    ice deleted

--- frontend/src/app.ts
+++
@@ -1,4 +0,0 @@
-import { defineAppConfig } from 'ice';
-
-// App config, see https://v3.ice.work/docs/guide/basic/app
-export default defineAppConfig(() => ({}));
--- frontend/src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Landing() {
-  return (
-    <div>
-      <h1>landing</h1>
-    </div>
-  );
-}
